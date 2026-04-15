e8e08ca111b31901acf91e8097df254cad37cdd4
shoh <shoh@orheun.com>
2026-04-15 17:25:57 +0900

    fix: 네비게이션 CSR -> SSR 로 변경

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -1,42 +1,18 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const Navigation = () => {
-  const router = useRouter();
-
   return (
-    <div className="fixed flex justify-center items-center top-0 w-screen h-[64px] bg-white">
+    <nav className="fixed flex justify-center items-center top-0 w-screen h-[64px] bg-white">
       <div className="flex justify-between w-full max-w-[1080px]">
         <p>올타</p>
 
         <div className="flex gap-[60px] font-medium">
-          <button
-            onClick={() => router.push("/about")}
-            className="cursor-pointer"
-          >
-            회사소개
-          </button>
-          <button
-            onClick={() => router.push("/service")}
-            className="cursor-pointer"
-          >
-            서비스
-          </button>
-          <button
-            onClick={() => router.push("/solution")}
-            className="cursor-pointer"
-          >
-            솔루션
-          </button>
-          <button
-            onClick={() => router.push("/news")}
-            className="cursor-pointer"
-          >
-            뉴스룸
-          </button>
+          <Link href="/about">회사소개</Link>
+          <Link href="/service">서비스</Link>
+          <Link href="/solution">솔루션</Link>
+          <Link href="/news">뉴스룸</Link>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
