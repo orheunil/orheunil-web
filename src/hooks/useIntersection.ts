0df799b211755f950f97a3cb08149ec4191a1dab
shoh <shoh@orheun.com>
2026-05-04 09:37:11 +0900

    feat: 섹션 관측 커스텀훅

---
+++ src/hooks/useIntersection.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useIntersection = (threshold = 0.75) => {
+  const ref = useRef<HTMLElement | null>(null);
+
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.intersectionRatio > threshold) {
+          setIsIntersecting(true);
+        }
+      },
+      { threshold },
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [threshold]);
+
+  return { ref, isIntersecting };
+};
