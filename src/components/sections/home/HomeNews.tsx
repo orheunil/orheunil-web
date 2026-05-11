5ba6523bfdba7f9658d5abf4e7af8182fba44d86
shoh <shoh@orheun.com>
2026-05-11 14:31:46 +0900

    fix: 홈 화면 뉴스섹션 반응형 UI

--- src/components/sections/home/HomeNews.tsx
+++ src/components/sections/home/HomeNews.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { HoverWrapper } from "@/components/animation/Hover";
 import { newsList } from "@/contants";
+import Link from "next/link";
 
 interface Props {
   t: (key: string) => string;
@@ -11,15 +12,17 @@ export const HomeNews = ({ t }: Props) => {
     <section className="flex justify-center w-full px-[60px] py-[140px] bg-white">
       <div className="flex flex-col w-full max-w-[1080px]">
         <HoverWrapper>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <h2 className="text-[40px] font-semibold">최근 소식</h2>
 
-            <button>더보기</button>
+            <div className="flex justify-center items-center px-[16px] py-[8px] text-white text-[16px] font-semibold bg-black rounded-[50px]">
+              더보기
+            </div>
           </div>
         </HoverWrapper>
 
         <HoverWrapper distance={40}>
-          <div className="flex mt-[60px] gap-[40px]">
+          <div className="flex flex-col lg:flex-row mt-[60px] gap-[40px]">
             {newsList
               .sort((a, b) => b.index - a.index)
               .map((value, index) => (
@@ -28,7 +31,7 @@ export const HomeNews = ({ t }: Props) => {
                   href={value.url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex flex-col flex-1 cursor-pointer"
+                  className="flex flex-col md:flex-row lg:flex-col flex-1 items-center gap-[32px] cursor-pointer"
                 >
                   <div
                     style={{
@@ -36,15 +39,17 @@ export const HomeNews = ({ t }: Props) => {
                       backgroundPosition: "center",
                       backgroundSize: "cover",
                     }}
-                    className="w-full h-[206px] rounded-[16px]"
+                    className="w-full md:w-[324px] lg:w-full h-[206px] rounded-[16px]"
                   />
 
-                  <h3 className="mt-[32px] text-[20px] font-semibold line-clamp-2">
-                    {value.title}
-                  </h3>
-                  <p className="mt-[12px] text-gray3 text-[16px]">
-                    {value.date}
-                  </p>
+                  <div className="flex flex-col flex-1">
+                    <h3 className=" text-[20px] font-semibold line-clamp-2">
+                      {value.title}
+                    </h3>
+                    <p className="mt-[12px] text-gray3 text-[16px]">
+                      {value.date}
+                    </p>
+                  </div>
                 </a>
               ))}
           </div>
