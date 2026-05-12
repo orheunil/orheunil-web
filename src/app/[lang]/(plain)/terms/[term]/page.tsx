27bcdae5e0bc312a811fad281a8d695ce3c226db
shoh <shoh@orheun.com>
2026-05-12 11:20:43 +0900

    fix: 검색엔진 색인 제외 위치 변경

--- src/app/[lang]/(plain)/terms/[term]/page.tsx
+++ src/app/[lang]/(plain)/terms/[term]/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
 import { terms } from "@/contants";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default async function Terms({
   params,
